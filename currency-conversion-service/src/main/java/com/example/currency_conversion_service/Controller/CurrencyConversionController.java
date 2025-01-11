9a1d3f089cdbbc384bf926a1fd3e5b500a08d6fa
AlejandroDelg <alexdelga2013@gmail.com>
2025-01-11 15:29:41 +0100

    Added retrieveQuantity function

--- currency-conversion-service/src/main/java/com/example/currency_conversion_service/Controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/currency_conversion_service/Controller/CurrencyConversionController.java
@@ -1,4 +1,19 @@
 package com.example.currency_conversion_service.Controller;
 
+import com.example.currency_conversion_service.Entity.CurrencyConversion;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
 public class CurrencyConversionController {
+
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion retrieveQuantity(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
+        return new CurrencyConversion(1001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+    }
+
 }
